9bc79bce07350bd773e82c2306e50cc102b468e1
phamquocvuong <phamquocvuongtt19@gmail.com>
2023-04-01 12:51:52 +0700

    viet them duong dan den trong login

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,34 @@
 var express = require("express");
 var router = express.Router();
-
 const clonePhim4400 = require("../src/clonePhim4400");
 const hostClone = require("../src/config/hostClone");
+const checkAdminLogin = require("../src/middleware/checkAdminLogin");
 
 const MovieSerieModel = require("../src/db/model/MovieSerie");
 const MovieModel = require("../src/db/model/Movie");
 const MovieOptionModel = require("../src/db/model/MovieOption");
 const RegionModel = require("../src/db/schema/RegionSchema");
-
+var sessionstorage = require("sessionstorage");
 const config = require("../src/config");
 const common = require("../src/common");
 //Page trang chu
-router.get("/quamon", async (req, res, next) =>{
+router.get("/quamon",  (req, res, next) =>{
   res.render("quamon");
 })
-router.get("/", async (req, res, next) => {
-  //Phim moi
-  const topMovies = MovieModel.getTopMovie(config.itemPerPage);
-  //Phim hanh dong
-  const hotMovies = MovieModel.findMovieByCategory(
-    "phim-hanh-dong",
-    config.itemPerPage,
-    0
-  );
-  //Phim viet
-  const vietMovies = MovieModel.findMovieByCategory("pr", config.itemPerPage);
-  //Danh sach phim bo
-  const seriaMovies = MovieSerieModel.getTopListSerieMovie(config.itemPerPage);
-  const menu = MovieModel.getMenu();
-  let [resTopMV, resHotMV, resSeriaMV, resVietMV, resMenu] = await Promise.all([
-    topMovies,
-    hotMovies,
-    seriaMovies,
-    vietMovies,
-    menu,
-  ]);
-  res.render("index", {
-    title: "Movies",
-    topMovies: resTopMV,
-    hotMovies: resHotMV,
-    seriaMovies: resSeriaMV,
-    vietMovies: resVietMV,
-    movie: null,
-    menu: resMenu,
-  });
-});
 
-//Page thong tin phim
+router.get("/",  (req, res, next) =>{
+  res.render("login");
+})
+
+router.post("/",  function (req, res, next) {
+  const { username, password } = req.body;
+  console.log({username, password});
+  if (username == "1" && password == "1") {
+    res.redirect("/admin")
+  } else {
+    res.render("login");
+  }
+});
 router.get("/phim/:slug", async (req, res, next) => {
   const { slug } = req.params;
   const movie = MovieModel.findMovieBySlug(slug);
